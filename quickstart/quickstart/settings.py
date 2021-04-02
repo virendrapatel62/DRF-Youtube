21946d0abc4dc1bd763e46b218e72f99c1cc042c
virendra <patelvirendra62@gmail.com>
2021-04-02 09:55:23 +0530

    mysql connection

--- quickstart/quickstart/settings.py
+++ quickstart/quickstart/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'quickstart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "quickstart",
+        "HOST" : 'localhost', 
+        "PORT" : 3306, 
+        "USER" : 'root', 
+        'PASSWORD' : 'root'
     }
 }
 
