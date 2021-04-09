9cfde76a882585be00d7efe92064637d965cba11
virendra <patelvirendra62@gmail.com>
2021-04-09 21:28:02 +0530

    class Base View NO PK

--- CBVproject/CBVapp/models.py
+++ CBVproject/CBVapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from rest_framework import serializers
 # Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
+    author = models.CharField(max_length=40)
+    price = models.IntegerField()
+    discount = models.IntegerField(default=0)
+    duration= models.FloatField()
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields ="__all__"
--- CBVproject/CBVapp/views.py
+++ CBVproject/CBVapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Course , CourseSerializer
 
-# Create your views here.
+class CourseListView(APIView):
+    def get(self , request ):
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses , many = True)
+        return Response(serializer.data)
+    
+    def post(self , request):
+        courseSerializer = CourseSerializer(data = request.data)
+        if courseSerializer.is_valid():
+            courseSerializer.save()
+            return Response(courseSerializer.data , status=status.HTTP_201_CREATED)
+        
+        return Response(courseSerializer.errors)
+        
