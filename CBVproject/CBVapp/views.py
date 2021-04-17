e3fee48d0fdb171b24f0daf0b249a4f8c513aa9a
virendra <patelvirendra62@gmail.com>
2021-04-17 10:12:16 +0530

    Signed-off-by: virendra <patelvirendra62@gmail.com>

--- CBVproject/CBVapp/views.py
+++ CBVproject/CBVapp/views.py
@@ -3,44 +3,74 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
-from .models import Course , CourseSerializer
+from .models import Course, CourseSerializer
+from rest_framework import mixins, generics
 
-class CourseListView(APIView):
-    def get(self , request ):
-        courses = Course.objects.all()
-        serializer = CourseSerializer(courses , many = True)
-        return Response(serializer.data)
+
+class CourseListView(mixins.ListModelMixin, mixins.CreateModelMixin,  generics.GenericAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+
+class CourseDetailView(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
     
-    def post(self , request):
-        courseSerializer = CourseSerializer(data = request.data)
+    def get(self, request, pk):
+        return self.retrieve(request, pk)
+
+    def put(self, request, pk):
+        return self.update(request, pk)
+
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
+
+
+'''
+class CourseListView(APIView):
+    def get(self, request):
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        courseSerializer = CourseSerializer(data=request.data)
         if courseSerializer.is_valid():
             courseSerializer.save()
-            return Response(courseSerializer.data , status=status.HTTP_201_CREATED)
-        
+            return Response(courseSerializer.data, status=status.HTTP_201_CREATED)
+
         return Response(courseSerializer.errors)
-        
+
+
 class CourseDetailView(APIView):
 
-    def get_course(self , pk ):
+    def get_course(self, pk):
         try:
-            return Course.objects.get(pk = pk)
+            return Course.objects.get(pk=pk)
         except Course.DoesNotExist:
             raise Http404
 
-    def get(self , request , pk):
+    def get(self, request, pk):
         course = self.get_course(pk)
-        serilizer = CourseSerializer(course);
+        serilizer = CourseSerializer(course)
         return Response(serilizer.data)
 
-    
-    def delete(self , request , pk):
+    def delete(self, request, pk):
         self.get_course(pk).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
-    def put(self , request , pk):
+
+    def put(self, request, pk):
         course = self.get_course(pk)
-        courseSerializer = CourseSerializer(course , data = request.data)
+        courseSerializer = CourseSerializer(course, data=request.data)
         if courseSerializer.is_valid():
             courseSerializer.save()
             return Response(courseSerializer.data)
         return Response(courseSerializer.errors)
+
+'''
