2496b0b8f559201acda80c652163bf9d1216d4af
virendra <patelvirendra62@gmail.com>
2021-05-23 12:28:37 +0530

    basic authentication

--- nsproject/nsapp/views.py
+++ nsproject/nsapp/views.py
@@ -1,10 +1,24 @@
 
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import authentication
 from .serializers import InstructorSerializer, CourseSerializer
 from .models import Instructor, Course
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, BasePermission
 from rest_framework.authentication import BasicAuthentication
+
+
+class WriteByAdminOnlyPermission(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        if request.method == 'GET':
+            return True
+
+        if request.method == 'POST' or request.method == 'PUT' or request.method == 'DELETE':
+            if user.is_superuser:
+                return True
+
+        return False
 
 
 class InstructorListView(generics.ListCreateAPIView):
@@ -19,7 +33,7 @@ class InstructorDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 class CourseListView(generics.ListCreateAPIView):
     authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, WriteByAdminOnlyPermission]
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
